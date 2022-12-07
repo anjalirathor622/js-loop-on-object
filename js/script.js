cc9fb4f9baeab8f04751ad266aa155b7068100b0
anjalirathor622 <anjalirathor622@gmail.com>
2022-12-07 07:07:11 +0530

    ok

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// externel javascript
+
+//console.log("esternal js")
+
+// let variable
+
+let x ;   //declaring a variable
+x = 10  //initilizing vatiable
+console.log('type of x is '+typeof x);   //number
+
+//let x = "anjali" //we cant redeclare let variable
+
+x = "i am fine";    //we can re initiilize let variable
+console.log(x);
+
+// "const" variable
+const y ="welcome!"   //we have to declare and initilize const variable togather
+
+console.log(y);  //we cant redeclare or rinitilize const variable
+
+document.write("<i>"+y+"</i>");
